f6112099cae1d271280dc411cd0faf82ae3ec4a1
DebasishKisan1 <debasish.kisan@gmail.com>
2024-02-07 22:14:46 +0530

    initial

---
+++ reduxdemo.js
@@ -0,0 +1,32 @@
+const redux = require('redux')
+
+const counterReducer = (state = { counter: 0 }, action) => {
+    if (action.type === 'increment') {
+        return {
+            counter: state.counter + 5
+        }
+    }
+
+    if (action.type === 'decrement') {
+
+        return {
+            counter: state.counter - 5
+        }
+    }
+}
+
+const store = redux.createStore(counterReducer)
+
+const counterSubscriber = () => {
+    const lateststate = store.getState()
+    console.log(lateststate)
+}
+
+store.subscribe(counterSubscriber)
+
+store.dispatch({ type: 'increment' })
+store.dispatch({ type: 'increment' })
+store.dispatch({ type: 'increment' })
+store.dispatch({ type: 'increment' })
+store.dispatch({ type: 'increment' })
+store.dispatch({ type: 'decrement' })
