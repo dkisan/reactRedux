c3c3e5bbf2b76123443ca786b1210834147b0685
DebasishKisan1 <debasish.kisan@gmail.com>
2024-02-08 12:17:37 +0530

    TDD useractions

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render } from "@testing-library/react"
 import App from "./App"
+import CartButton from "./components/Cart/CartButton"
 
 test('render',()=>{
     render(<App/>)
     const text = screen.getByText('Redux Cart')
     expect(text).toBeInTheDocument()
 })
+
+test('renders "Changed!" if the button was clicked', () => {
+    // Arrange
+    render(<CartButton />);
+
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement)
+
+    // Assert
+    const outputElement = screen.getByText('Sending!');
+    expect(outputElement).toBeInTheDocument();
+  });
